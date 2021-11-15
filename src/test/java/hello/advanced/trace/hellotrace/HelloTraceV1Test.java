d97886eee97ebdab6d1d700b78b64332f6549277
pdh8788 <pdh1753@gmail.com>
2021-11-15 13:39:54 +0900

    Changes

--- src/test/java/hello/advanced/trace/hellotrace/HelloTraceV1Test.java
+++ src/test/java/hello/advanced/trace/hellotrace/HelloTraceV1Test.java
@@ -32,4 +32,11 @@ public class HelloTraceV1Test {
         TraceStatus status = trace.begin("hello");
         trace.exception(status, new IllegalStateException());
     }
+
+    @Test
+    void begin_exception3() {
+        HelloTraceV1 trace = new HelloTraceV1();
+        TraceStatus status = trace.begin("hello");
+        trace.exception(status, new IllegalStateException());
+    }
 }
